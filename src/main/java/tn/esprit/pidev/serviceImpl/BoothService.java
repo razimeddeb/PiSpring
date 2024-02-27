f3c5d19893517fd7ba483b5312f4c72bfb12c5f0
razimeddeb <razi.elmeddebjaouadi@esprit.tn>
2024-02-27 13:02:02 +0100

    commit 2

---
+++ src/main/java/tn/esprit/pidev/serviceImpl/BoothService.java
@@ -0,0 +1,40 @@
+package tn.esprit.pidev.serviceImpl;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import tn.esprit.pidev.entity.Booth;
+import tn.esprit.pidev.repository.BoothRepository;
+import tn.esprit.pidev.serviceIterface.IBoothService;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class BoothService implements IBoothService {
+    BoothRepository boothRepository;
+    @Override
+    public Booth addBooth(Booth booth) {
+        return boothRepository.save(booth);
+    }
+
+    @Override
+    public List<Booth> retriveAllBooths() {
+        return boothRepository.findAll();
+    }
+
+    @Override
+    public Booth updateBooth(Booth booth) {
+        return boothRepository.save(booth);
+    }
+
+    @Override
+    public Booth retriveBooth(Long idBooth) {
+        return boothRepository.findById(idBooth).orElse(null);
+    }
+
+    @Override
+    public void removeBooth(Long idBooth) {
+        boothRepository.deleteById(idBooth);
+
+    }
+}
